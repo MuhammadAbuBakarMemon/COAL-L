7554ca3769791ced83da77030b0129980658dbdd
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2025-11-30 03:08:19 +0500

    Update Q1.asm

--- LabTasks/Lab05/Q1.asm
+++ LabTasks/Lab05/Q1.asm
@@ -1,1 +1,40 @@
+INCLUDE Irvine32.inc 
 
+.data 
+
+.code 
+main PROC 
+
+COMMENT ! 
+MOV al, 7Fh
+ADD al, 1
+
+CALL DumpRegs
+
+Values of CPU Status Flag Registers  (EFLAGS)
+ZF = 0
+CF = 0
+SF = 1
+AC = 1
+PF = 0
+OF = 0
+!
+
+MOV al, 7Fh
+SUB al, 80h
+
+CALL DumpRegs
+
+COMMENT ! 
+Values of CPU Status Flag Registers  (EFLAGS)
+ZF = 0
+CF = 0
+SF = 1
+AC = 0
+PF = 1
+OF = 1
+!
+
+exit 
+main ENDP 
+END main
