6134ce69b82123658d90359f0582b91af75100f2
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2025-11-30 04:26:17 +0500

    Update Q1.asm

--- LabTasks/LAB06/Q1.asm
+++ LabTasks/LAB06/Q1.asm
@@ -1,1 +1,29 @@
+INCLUDE Irvine32.inc 
 
+.data 
+
+.data
+m1 BYTE "Welcome", 0
+m2 BYTE "You should not see this line", 0
+m3 BYTE "Goodbye", 0
+
+.code 
+main PROC 
+
+MOV edx, OFFSET m1
+CALL WriteString 
+CALL Crlf 
+
+JMP skip
+MOV edx, OFFSET m2
+CALL WriteString 
+CALL Crlf
+
+skip:
+MOV edx, OFFSET m3
+CALL WriteString 
+CALL Crlf
+
+exit 
+main ENDP 
+END main
